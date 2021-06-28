13db4d6decf8bf863607545028f8899b284cb5b8
Azamat Maratov <aza2001.05.16.gmail.com>
2021-06-29 01:25:49 +0600

    Updates

---
+++ precache-manifest.841ce0d90cd13218d01e738c760d3e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae3acfd0b565ba12ea1609d7fff0dfe",
+    "url": "/pomodoro-redux/index.html"
+  },
+  {
+    "revision": "61762e7b72f6c0b88a07",
+    "url": "/pomodoro-redux/static/css/main.d40d8e14.chunk.css"
+  },
+  {
+    "revision": "2c6b6551e1a3895ab044",
+    "url": "/pomodoro-redux/static/js/2.8d744b23.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/pomodoro-redux/static/js/2.8d744b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61762e7b72f6c0b88a07",
+    "url": "/pomodoro-redux/static/js/main.843536c7.chunk.js"
+  },
+  {
+    "revision": "17ed0dd787806e7a0f91",
+    "url": "/pomodoro-redux/static/js/runtime-main.1eb4b413.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/pomodoro-redux/static/media/bell.525320df.flac"
+  }
+]);
